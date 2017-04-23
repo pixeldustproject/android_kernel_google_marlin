546f93736636ee6ca347bd8961d66546e5095f0f
DespairFactor <matthewalex@linux.com>
2017-04-23 13:14:28 -0400

    build: R3.EXP

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ export CROSS_COMPILE=${HOME}/android/uberbuild/out/aarch64-linux-android-6.x/bin
 DEFCONFIG="sharkey_defconfig"
 
 # Kernel Details
-VER=".R2.EXP.Pixel"
+VER=".R3.EXP.Pixel"
 
 # Paths
 KERNEL_DIR=`pwd`
