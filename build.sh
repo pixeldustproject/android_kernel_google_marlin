80e82f39adb8a42e7edc0dec144dcb57fffcd060
DespairFactor <matthewalex@linux.com>
2017-05-01 19:16:58 -0400

    build: R6.EXP
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ export CROSS_COMPILE=${HOME}/android/uberbuild/out/aarch64-linux-android-6.x/bin
 DEFCONFIG="sharkey_defconfig"
 
 # Kernel Details
-VER=".R5.EXP.Pixel"
+VER=".R6.EXP.Pixel"
 
 # Paths
 KERNEL_DIR=`pwd`
